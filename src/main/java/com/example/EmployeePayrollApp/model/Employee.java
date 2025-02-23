fb1eae4f6b23ee9df64cd4707fada0f955a7fcdc
Om prakash <116386573+Omprakash556@users.noreply.github.com>
2025-02-23 23:33:20 +0530

    [Om Prakash]Add. Develop Employee Payroll Application Using Spring Boot

---
+++ src/main/java/com/example/EmployeePayrollApp/model/Employee.java
@@ -0,0 +1,24 @@
+package com.example.EmployeePayrollApp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private double salary;
+
+    private String department;
+}
